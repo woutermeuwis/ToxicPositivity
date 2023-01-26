88725ccf0a320ea3250be2b6d13d222e61d05004
Wouter Meuwis <wouter.meuwis@3factr.be>
2023-01-26 14:18:22 +0100

    implemented questions commands

--- src/commands/questions.js
+++ src/commands/questions.js
@@ -1,11 +1,70 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { defer, reply } = require('../helpers/interaction-helper')
+const { getGuildAsync, saveGuildAsync } = require('../helpers/guild-data')
+const { caseInsensitiveSort } = require('../helpers/sort-helper')
+const { EmbedBuilder } = require('discord.js')
+const { arrayIncludesString } = require('../helpers/array-helper')
 
-async function showQuestions (interaction) {}
+async function showQuestions (interaction) {
+  // parse interaction
+  const guild = await getGuildAsync(interaction.guildId)
+  const category = interaction.options.getString('category')
+  const questions = guild.questions[category]
 
-async function addNewQuestion (interaction) {}
+  if (questions.length === 0) {
+    await reply(interaction, `Er zijn nog geen vragen aangemaakt voor het type '${category}'!`)
+    return
+  }
 
-async function removeQuestion (interaction) {}
+  // build reply
+  const desc = questions
+    .sort(caseInsensitiveSort)
+    .map(list => `\u2022 ${list}`)
+    .join('\n')
+
+  const embed = new EmbedBuilder()
+    .setTitle(category)
+    .setDescription(desc)
+
+  // send reply
+  await reply(interaction, { content: ' ', embeds: [embed] })
+}
+
+async function addNewQuestion (interaction) {
+  // parse interaction
+  const guild = await getGuildAsync(interaction.guildId)
+  const category = interaction.options.getString('category')
+  const question = interaction.options.getString('question')
+  const existingQuestions = guild.questions[category]
+
+  // verify name
+  if (arrayIncludesString(existingQuestions, question, false)) {
+    await reply(interaction, `De vraag '${question}' bestaat al voor het type '${category}'!`)
+    return
+  }
+
+  guild.questions[category].push(question)
+  await saveGuildAsync(guild)
+
+  await reply(interaction, `De vraag '${question}' is aangemaakt voor het type '${category}'`)
+}
+
+async function removeQuestion (interaction) {
+  // parse interaction
+  const guild = await getGuildAsync(interaction.guildId)
+  const category = interaction.options.getString('category')
+  const question = interaction.op.getString('question')
+
+  if (!arrayIncludesString(guild.questions[category], question, false)) {
+    await reply(interaction, `De vraag '${question}' bestaat niet voor het type '${category}'!`)
+    return
+  }
+
+  guild.questions[category].remove(question)
+  await saveGuildAsync(guild)
+
+  await reply(interaction, `De vraag '${question}' is verwijderd uit het type '${category}'`)
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,13 +73,18 @@ module.exports = {
 
     .addSubcommand(sc => sc
       .setName('show')
-      .setDescription('Toon alle vragen per type'))
+      .setDescription('Toon alle vragen per type')
+      .addStringOption(so => so
+        .setName('category')
+        .setDescription('De category waar je de vragen van wil zien')
+        .setRequired(true)
+        .setAutocomplete(true)))
 
     .addSubcommand(sc => sc
       .setName('add')
       .setDescription('Voeg een nieuw vraag toe')
       .addStringOption(so => so
-        .setName('question_type')
+        .setName('category')
         .setDescription('Het type voor deze vraag')
         .setRequired(true)
         .setAutocomplete(true))
@@ -33,8 +97,13 @@ module.exports = {
       .setName('remove')
       .setDescription('Verwijder een vraag')
       .addStringOption(so => so
+        .setName('category')
+        .setDescription('De categorie waaruit je een vraag wil verwijderen')
+        .setRequired(true)
+        .setAutocomplete(true))
+      .addStringOption(so => so
         .setName('question')
-        .setDescription('De vraag die je wil verwijdern')
+        .setDescription('De vraag die je wil verwijderen')
         .setRequired(true)
         .setAutocomplete(true))),
 
--- src/helpers/autocomplete-helper.js
+++ src/helpers/autocomplete-helper.js
@@ -11,25 +11,47 @@ async function addAutocompleteOptions (interaction) {
     case 'categories':
       await addCategoriesAutocompleteOptions(interaction)
       break
+    case 'questions':
+      await addQuestionsAutocompleteOptions(interaction)
+      break
   }
 }
 
 async function addCategoriesAutocompleteOptions (interaction) {
   const { optionName, optionValue } = parseInteraction(interaction)
 
-  let data = null
   if (optionName === 'name') {
-    const guild = await getGuildAsync(interaction.guildId)
-    const existingCategories = Object.keys(guild.questions)
-    const filtered = existingCategories.filter(c => !optionValue || stringIncludes(c, optionValue, false))
-    if (filtered.length > 0) {
-      data = filtered
-        .sort(caseInsensitiveSort)
-        .slice(0, AUTOCOMPLETE_MAX_RESULTS)
-        .map(c => ({ name: c, value: c }))
+    const categories = await getAllCategoriesForGuild(interaction.guildId, optionValue)
+    await interaction.respond(categories)
+    return
+  }
+
+  await interaction.respond(null)
+}
+
+async function addQuestionsAutocompleteOptions (interaction) {
+  const { optionName, optionValue } = parseInteraction(interaction)
+  switch (optionName) {
+    case 'category': {
+      const categories = await getAllCategoriesForGuild(interaction.guildId, optionValue)
+      await interaction.respond(categories)
+      return
+    }
+    case 'question': {
+      // fetch chosen category
+      const category = interaction.options.data.find(o => o.type === ApplicationCommandOptionType.Subcommand).options.find(o => o.name === 'category').value
+      if (category) {
+        const questions = await getAllCategoryQuestionsForGuild(interaction.guildId, category, optionValue)
+        await interaction.respond(questions)
+        return
+      } else {
+        const questions = await getAllQuestionsForGuild(interaction.guildId, category, optionValue)
+        await interaction.respond(questions)
+        return
+      }
     }
   }
-  await interaction.respond(data)
+  await interaction.respond(null)
 }
 
 function parseInteraction (interaction) {
@@ -38,6 +60,53 @@ function parseInteraction (interaction) {
   const optionName = focusedOption.name
   const optionValue = focusedOption.value
   return { subCommand, optionName, optionValue }
+}
+
+async function getAllCategoriesForGuild (guildId, search) {
+  const guild = await getGuildAsync(guildId)
+  const existingCategories = Object.keys(guild.questions)
+  const filtered = existingCategories.filter(c => !search || stringIncludes(c, search, false))
+  if (filtered.length === 0) {
+    return null
+  } else {
+    return filtered
+      .sort(caseInsensitiveSort)
+      .slice(0, AUTOCOMPLETE_MAX_RESULTS)
+      .map(c => ({ name: c, value: c }))
+  }
+}
+
+async function getAllQuestionsForGuild (guildId, search) {
+  const guild = await getGuildAsync(guildId)
+  const questions = []
+  for (const cat of Object.values(guild.questions)) {
+    for (const q of cat) {
+      if (!search || stringIncludes(q, search, false)) {
+        questions.push(q)
+      }
+    }
+  }
+  if (questions.length === 0) {
+    return null
+  } else {
+    return questions
+      .sort(caseInsensitiveSort)
+      .slice(0, AUTOCOMPLETE_MAX_RESULTS)
+      .map(c => ({ name: c, value: c }))
+  }
+}
+
+async function getAllCategoryQuestionsForGuild (guildId, category, search) {
+  const guild = await getGuildAsync(guildId)
+  const questions = guild.questions[category].filter(q => !search || stringIncludes(q, search, false))
+  if (questions.length === 0) {
+    return null
+  } else {
+    return questions
+      .sort(caseInsensitiveSort)
+      .slice(0, AUTOCOMPLETE_MAX_RESULTS)
+      .map(c => ({ name: c, value: c }))
+  }
 }
 
 module.exports = { addAutocompleteOptions }
